Desbordante/desbordante-core

Support usage of bitset pair FD representation (TODO: remove inheritance)
doubt was because of reordering, decided result reporters will reorder

src/core/algorithms/fd/table_mask_pair.h
@@ -1,0 +1,29 @@
+#pragma once
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "core/algorithms/fd/fd.h"
+#include "core/model/index.h"
+#include "core/model/table/attribute.h"
+#include "core/model/table/table_header.h"
+
+namespace algos::fd {
+struct TableMaskPair {
+    boost::dynamic_bitset<> lhs;
+    boost::dynamic_bitset<> rhs;
+
+    model::FunctionalDependency ToFd(model::TableHeader const& masked_table_header) const {
+        std::vector<model::Attribute> lhs_attrs;
+        lhs_attrs.reserve(lhs.count());
+        util::ForEachIndex(lhs, [&](model::Index i) {
+            lhs_attrs.emplace_back(masked_table_header.column_names[i], i);
+        });
+        std::vector<model::Attribute> rhs_attrs;
+        rhs_attrs.reserve(rhs.count());
+        util::ForEachIndex(rhs, [&](model::Index i) {
+            rhs_attrs.emplace_back(masked_table_header.column_names[i], i);
+        });
+        return {masked_table_header.table_name, std::move(lhs_attrs), std::move(rhs_attrs)};
+    }
+};
+}  // namespace algos::fd

src/core/algorithms/fd/table_mask_pair_fd_view.h
@@ -1,0 +1,57 @@
+#pragma once
+
+#include <deque>
+#include <memory>
+#include <ranges>
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "core/algorithms/fd/fd.h"
+#include "core/algorithms/fd/table_mask_pair.h"
+#include "core/config/max_lhs/type.h"
+#include "core/model/table/table_header.h"
+#include "core/util/bitset_utils.h"
+
+namespace algos::fd {
+class TableMaskPairFdView {
+public:
+    // Use this, not the class itself, don't waste memory copying
+    using OwningPointer = std::shared_ptr<TableMaskPairFdView>;
+    using Storage = std::deque<TableMaskPair>;
+
+private:
+    model::TableHeader table_header_;
+    // A typical LHS doesn't use up that much memory, and a processable dataset doesn't usually have
+    // more than 256 columns. In lots of cases, we can store the numbers themselves compactly.
+    // Perhaps there is a use case for compressed storage in memory.
+    Storage table_mask_pairs_;
+
+    auto FdView() const {
+        return std::ranges::ref_view{table_mask_pairs_} |
+               std::views::transform(
+                       [table_header = &table_header_](TableMaskPair const& stripped_fd) {
+                           return stripped_fd.ToFd(*table_header);
+                       });
+    }
+
+public:
+    TableMaskPairFdView(model::TableHeader table_header, Storage stripped_fds)
+        : table_header_(std::move(table_header)), table_mask_pairs_(std::move(stripped_fds)) {}
+
+    Storage const& GetTableMaskPairs() const noexcept {
+        return table_mask_pairs_;
+    }
+
+    model::TableHeader const& GetTableHeader() const noexcept {
+        return table_header_;
+    }
+
+    auto begin() const {
+        return FdView().begin();
+    }
+
+    auto end() const {
+        return FdView().end();
+    }
+};
+}  // namespace algos::fd

wildsor/Desbordante

src/core/algorithms/fd/table_mask_pair.h
@@ -1,0 +1,30 @@
+#pragma once
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "core/algorithms/fd/functional_dependency.h"
+#include "core/model/index.h"
+#include "core/model/table/attribute.h"
+#include "core/model/table/table_header.h"
+#include "core/util/bitset_utils.h"
+
+namespace algos::fd {
+struct TableMaskPair {
+    boost::dynamic_bitset<> lhs;
+    boost::dynamic_bitset<> rhs;
+
+    model::FunctionalDependency ToFd(model::TableHeader const& masked_table_header) const {
+        std::vector<model::Attribute> lhs_attrs;
+        lhs_attrs.reserve(lhs.count());
+        util::ForEachIndex(lhs, [&](model::Index i) {
+            lhs_attrs.emplace_back(masked_table_header.column_names[i], i);
+        });
+        std::vector<model::Attribute> rhs_attrs;
+        rhs_attrs.reserve(rhs.count());
+        util::ForEachIndex(rhs, [&](model::Index i) {
+            rhs_attrs.emplace_back(masked_table_header.column_names[i], i);
+        });
+        return {masked_table_header.table_name, std::move(lhs_attrs), std::move(rhs_attrs)};
+    }
+};
+}  // namespace algos::fd

src/core/algorithms/fd/table_mask_pair_fd_view.h
@@ -1,0 +1,57 @@
+#pragma once
+
+#include <deque>
+#include <memory>
+#include <ranges>
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "core/algorithms/fd/functional_dependency.h"
+#include "core/algorithms/fd/table_mask_pair.h"
+#include "core/config/max_lhs/type.h"
+#include "core/model/table/table_header.h"
+#include "core/util/bitset_utils.h"
+
+namespace algos::fd {
+class TableMaskPairFdView {
+public:
+    // Use this, not the class itself, don't waste memory copying
+    using OwningPointer = std::shared_ptr<TableMaskPairFdView>;
+    using Storage = std::deque<TableMaskPair>;
+
+private:
+    model::TableHeader table_header_;
+    // A typical LHS doesn't use up that much memory, and a processable dataset doesn't usually have
+    // more than 256 columns. In lots of cases, we can store the numbers themselves compactly.
+    // Perhaps there is a use case for compressed storage in memory.
+    Storage table_mask_pairs_;
+
+    auto FdView() const {
+        return std::ranges::ref_view{table_mask_pairs_} |
+               std::views::transform(
+                       [table_header = &table_header_](TableMaskPair const& stripped_fd) {
+                           return stripped_fd.ToFd(*table_header);
+                       });
+    }
+
+public:
+    TableMaskPairFdView(model::TableHeader table_header, Storage stripped_fds)
+        : table_header_(std::move(table_header)), table_mask_pairs_(std::move(stripped_fds)) {}
+
+    Storage const& GetTableMaskPairs() const noexcept {
+        return table_mask_pairs_;
+    }
+
+    model::TableHeader const& GetTableHeader() const noexcept {
+        return table_header_;
+    }
+
+    auto begin() const {
+        return FdView().begin();
+    }
+
+    auto end() const {
+        return FdView().end();
+    }
+};
+}  // namespace algos::fd